622e1c3dc019bfdfc1a2eb65bae2475eab6a9794
Martin Barri <martin.barri@hotmail.com>
2012-02-22 13:15:29 +0100

    Changed SpaceInvaders22 to SpaceInvaders

--- src/com/spaceinvaders/SpaceInvadersActivity.java
+++ src/com/spaceinvaders/SpaceInvadersActivity.java
@@ -3,7 +3,7 @@ package com.spaceinvaders;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SpaceInvaders22Activity extends Activity {
+public class SpaceInvadersActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
