6436830f99adcd7333ed4846c5836d86cd5f02ea
nli <anneli.halldn@student.kyh.se>
2012-02-23 13:34:36 +0100

    monster can move

--- src/com/spaceinvaders/Monster.java
+++ src/com/spaceinvaders/Monster.java
@@ -1,14 +1,51 @@
 package com.spaceinvaders;
 
+
+import com.spaceinvaders.Panel;
 import android.graphics.Bitmap;
 
 public class Monster extends EntityPic {
+	
+	int x;
+	int y;
+	int xMov;
+	int yMov;
+	Bitmap bitmap;
+	
 	
 	//rörelsemönster
 	
 	
 	public Monster(int x, int y, Bitmap bitmap){
 		super(x,y, bitmap);
+		this.x = x;
+		this.y = y;
+		this.bitmap = bitmap;
+		xMov = 3;
+		yMov = 0;
+		
+	}
+	@Override
+	public void updatePosition(){
+		
+		
+		if(getCenterX() > Panel.screenWidth){
+			xMov = - 3;
+		}else if(getCenterX() < 0){
+			xMov = 3;
+		}
+		
+		if (getyPos() <= 0){
+		
+			setxPos(Panel.screenWidth /2);
+			
+			
+		}else if(getyPos() >= Panel.screenHeight){
+			
+			setxPos(Panel.screenWidth /2);
+			
+		}
+		movement(xMov, yMov);
 	}
 
 }
